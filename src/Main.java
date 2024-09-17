9edad6fdb877f5b45b1df401b1645aae6842abf0
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2024-09-17 10:49:43 -0400

    Update Main.java
    9/17/24

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println("| Note! Do not use any symbols when inputting data! (no $ or %) |");
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println();
+
+        System.out.print("Enter your Bill($): ");
+        String total_bill = s.nextLine();
+        double bill = Double.parseDouble(total_bill);
+
+        System.out.print("Tip Percent: ");
+        String tip_percent = s.nextLine();
+        double tip = Integer.parseInt(tip_percent);
+
+        System.out.print("Number of People: ");
+        String people = s.nextLine();
+        int number = Integer.parseInt(people);
+        System.out.println();
+
+        double tip_amount = (tip/100) * bill;
+        double bill_cost = bill + tip_amount;
+        double tip_per_person = tip_amount/number;
+        double bill_per_person = bill_cost/number;
+
+
+        System.out.println("-----------------------------");
+        System.out.println("| Total Tip Amount: $" + tip_amount + "|");
+        System.out.println("-----------------------------");
+        System.out.println("| Total Bill Cost: $" + bill_cost + "|");
+        System.out.println("-----------------------------");
+        System.out.println("| Tip per person: $" + tip_per_person + "|");
+        System.out.println("-----------------------------");
+        System.out.println("| Total bill per person: $" + bill_per_person + "|");
+        System.out.println("-----------------------------");
     }
 }
